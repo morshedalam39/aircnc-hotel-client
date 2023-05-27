b597070103d74c3b8f5073a2f3b3f76b027d33f8
morshedalam39 <morshedalam4239@gmail.com>
2023-05-27 23:08:27 +0600

    category Added

---
+++ src/components/Categoris/Categoris.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categoris = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Categoris;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
+import Home from '../pages/Home/Home'
+import Logo from '../components/Shared/Navbar/Logo'
+import SignUp from '../pages/SignUp/SignUp'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
+  },
+  {
+    path:'/login',
+    element:<Logo></Logo>
+  },
+  {
+    path:'/SignUp',
+    element:<SignUp></SignUp>
   },
 ])
